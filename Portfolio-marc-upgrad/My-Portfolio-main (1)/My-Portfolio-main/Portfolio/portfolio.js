7f67b28c5bb881c510a07b212f5e5752d4a84fd5
Jean-François Colin <jeanfrancois.colin8@gmail.Com>
2022-08-16 18:03:17 +0200

    tr

--- Portfolio-marc-upgrad/My-Portfolio-main (1)/My-Portfolio-main/Portfolio/portfolio.js
+++
@@ -1,7 +0,0 @@
-const backP = document.querySelector(".backPage");
-const mouseMv = document.querySelector(".mouseMove");
-
-backP.addEventListener("mousemove", (e) => {
-  mouseMv.style.left = (e.x / window.innerWidth) * 100 + "%";
-  mouseMv.style.top = (e.y / window.innerHeight) * 100 + "%";
-});
